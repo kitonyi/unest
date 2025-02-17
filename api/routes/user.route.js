a8e71bad7f42df9438e5a2d47b2c5d808b785428
kitonyi <kitonyi@gmail.com>
2025-02-17 13:48:26 +0300

    Added user controller n route

---
+++ api/routes/user.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import { test } from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+// router.get("/test", (req, res) => {
+//   res.json({
+//     message: "Hello world",
+//   });
+// });
+
+router.get("/test", test);
+
+export default router;
