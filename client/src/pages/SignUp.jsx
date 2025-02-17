09658d6831f05739908ab8edc190c33fcdf86bef
kitonyi <kitonyi@gmail.com>
2025-02-17 15:50:32 +0300

    Added sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          id="username"
+          placeholder="username"
+          className="border p-3 rounded-lg focus:outline-none"
+        />
+        <input
+          type="email"
+          id="email"
+          placeholder="email"
+          className="border p-3 rounded-lg focus:outline-none"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="password"
+          className="border p-3 rounded-lg focus:outline-none"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-75">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to="/sign-in">
+          {" "}
+          <span className="text-blue-700">Sign In</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
