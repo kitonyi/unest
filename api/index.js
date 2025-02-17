e7f3c259010a824001bf0317799fb6e12170e48a
kitonyi <kitonyi@gmail.com>
2025-02-17 14:31:46 +0300

    Created signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -14,6 +15,7 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
@@ -26,3 +28,4 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
